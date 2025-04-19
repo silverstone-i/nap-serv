bf47441ac363c896cd428bbfbb61c17743674136
ian <ian@isilverstone.com>
2025-04-19 17:52:42 -0500

    feat: add database connection and repositories setup

---
+++ src/db/db.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/*
+ * Copyright © 2024-present, Ian Silverstone
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ */
+
+import { DB } from 'pg-schemata';
+import repositories from './repositories.js';
+
+console.log('Initializing database connection...');
+const { db } = DB.init(process.env.DATABASE_URL, repositories);
+console.log('Database connection established.');
+
+export default db;
