648f3746ae1e37a8234f8a8093eede005c9180cd
ian <ian@isilverstone.com>
2025-04-20 12:05:44 -0500

    fix: reorder tenantRepositories for improved loading control. This is a short term solution. See TODO for more details.

--- Modules/tenants/tenantRepositories.js
+++ Modules/tenants/tenantRepositories.js
@@ -1,20 +1,26 @@
 'use strict';
 
 /*
-* Copyright © 2024-present, Ian Silverstone
-*
-* See the LICENSE file at the top-level directory of this distribution
-* for licensing information.
-*
-* Removal or modification of this copyright notice is prohibited.
-*/
+ * Copyright © 2024-present, Ian Silverstone
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ */
 
 import Tenants from './models/Tenants.js';
 import NapUsers from './models/NapUsers.js';
 
+// TODO: implement a more robust way to handle this.
+//
+// We need more control over the order that repositories are loaded so we can take constraints into account.
+// For example, we need to ensure that the NapUsers table is created before the Tenants table.
+// This is because the NapUsers table is used by the Tenants table to store the user_id column.
+// Further we need control over which tables are loaded for a tenant so that only the modules they need are deployed
 const tenantRepositories = {
-  tenants: Tenants,
   napUsers: NapUsers,
+  tenants: Tenants,
 };
 
 export default tenantRepositories;
