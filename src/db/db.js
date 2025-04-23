8c12cf6f2dbb16e3300bbc778130b8ee170e898c
ian <ian@isilverstone.com>
2025-04-22 21:45:15 -0400

    fix: update database connection initialization to use environment-specific DATABASE_URL

--- src/db/db.js
+++ src/db/db.js
@@ -13,8 +13,30 @@ import 'dotenv/config'
 import { DB } from 'pg-schemata';
 import repositories from './repositories.js';
 
+let DATABASE_URL;
+switch (process.env.NODE_ENV) {
+  case 'development':
+    DATABASE_URL = process.env.DATABASE_URL_DEV;
+    break;
+  case 'test':
+    DATABASE_URL = process.env.DATABASE_URL_TEST;
+    break;
+  // TODO: Uncomment when ready for production  
+  // case 'production':
+  //   DATABASE_URL = process.env.DATABASE_URL_PROD;
+  //   break;
+  default:
+    throw new Error('NODE_ENV is not set to a valid value');
+}
+
+console.log('DATABASE_URL:', DATABASE_URL);
+if (!DATABASE_URL) {
+  throw new Error('DATABASE_URL is not set');
+}
+
+
 console.log('\nInitializing database connection...');
-const { db, pgp } = DB.init(process.env.DATABASE_URL, repositories);
+const { db, pgp } = DB.init(DATABASE_URL, repositories);
 console.log('Database connection established.\n');
 
 export { db, pgp };
