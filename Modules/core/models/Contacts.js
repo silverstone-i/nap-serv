bd0f7b18ff65468840f077aae3f8e187737499b0
ian <ian@isilverstone.com>
2025-04-30 23:02:34 -0400

    feat: add Contacts model and schema for contact management

---
+++ Modules/core/models/Contacts.js
@@ -0,0 +1,20 @@
+'use strict';
+
+/*
+* Copyright © 2024-present, Ian Silverstone
+*
+* See the LICENSE file at the top-level directory of this distribution
+* for licensing information.
+*
+* Removal or modification of this copyright notice is prohibited.
+*/
+
+import { BaseModel } from 'pg-schemata';
+import contactsSchema from '../schemas/contactsSchema.js';
+
+class Contacts extends BaseModel {
+  constructor(db, pgp) {
+    super(db, pgp, contactsSchema);
+  }
+}
+export default Contacts;
