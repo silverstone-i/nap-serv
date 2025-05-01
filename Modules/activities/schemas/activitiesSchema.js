703d333a5e54226c4def88cf1252e368c3c977ab
ian <ian@isilverstone.com>
2025-04-30 23:01:46 -0400

    feat: implement ActivityBudgets model and update activities schema with new constraints and columns

--- Modules/activities/schemas/activitiesSchema.js
+++ Modules/activities/schemas/activitiesSchema.js
@@ -1,39 +1,62 @@
 'use strict';
 
 /*
-* Copyright © 2024-present, Ian Silverstone
-*
-* See the LICENSE file at the top-level directory of this distribution
-* for licensing information.
-*
-* Removal or modification of this copyright notice is prohibited.
-*/
+ * Copyright © 2024-present, Ian Silverstone
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ */
 
-const activitiesSchema = {
+const schema = {
   dbSchema: 'tenantid',
   table: 'activities',
   hasAuditFields: true,
   version: '1.0.0',
-  columns: [
-    { name: 'id', type: 'uuid', default: 'uuid_generate_v4()', nullable: false, immutable: true, colProps: { cnd: true }},
-    { name: 'activity_id', type: 'varchar', length: 10, nullable: false, colProps: { cnd: true }},
-    { name: 'name', type: 'varchar', length: 255, nullable: false },
-    { name: 'category_id', type: 'uuid', nullable: false, colProps: { cnd: true }},
-    { name: 'type', type: 'varchar', length: 50, nullable: false, default: `'turnkey'` },
-    { name: 'description', type: 'text', nullable: true }
-  ],
   constraints: {
     primaryKey: ['id'],
     foreignKeys: [
       {
         type: 'ForeignKey',
-        columns: ['category_id'],
-        references: { table: 'tenantid.categories', columns: ['id'] },
-        onDelete: 'CASCADE'
-      }
+        columns: ['tenant_id', 'category_id'],
+        references: {
+          table: 'tenantid.categories',
+          columns: ['tenant_id', 'category_id'],
+        },
+        onDelete: 'CASCADE',
+      },
     ],
-    indexes: [{ type: 'Index', columns: ['category_id', 'type'] }]
-  }
+    unique: [['tenant_id', 'activity_id']],
+    indexes: [
+      {
+        type: 'Index',
+        columns: ['tenant_id', 'category_id'],
+      },
+    ],
+  },
+
+  columns: [
+    {
+      name: 'id',
+      type: 'uuid',
+      default: 'uuidv7()',
+      nullable: false,
+      immutable: true,
+      colProps: { cnd: true },
+    },
+    { name: 'tenant_id', type: 'uuid', nullable: false },
+    { name: 'activity_id', type: 'varchar(12)', nullable: false },
+    { name: 'name', type: 'varchar(32)', nullable: false },
+    { name: 'category_id', type: 'varchar(12)', nullable: false },
+    {
+      name: 'type',
+      type: 'varchar(12)',
+      nullable: false,
+      default: `'turnkey'`,
+    },
+    { name: 'description', type: 'text', nullable: true },
+  ],
 };
 
-export default activitiesSchema;
+export default schema;
