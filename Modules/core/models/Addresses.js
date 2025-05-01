03ad59e6f33af2df683bb064b5d8a8f03cb6ba2f
ian <ian@isilverstone.com>
2025-04-30 23:02:11 -0400

    feat: add Addresses model and schema for address management

---
+++ Modules/core/models/Addresses.js
@@ -0,0 +1,20 @@
+'use strict';
+
+/*
+* Copyright © 2024-present, Ian Silverstone
+*
+* See the LICENSE file at the top-level directory of this distribution
+* for licensing information.
+*
+* Removal or modification of this copyright notice is prohibited.
+*/
+
+import { BaseModel } from 'pg-schemata';
+import addressesSchema from '../schemas/addressesSchema.js';
+
+class Addresses extends BaseModel {
+  constructor(db, pgp) {
+    super(db, pgp, addressesSchema);
+  }
+}
+export default Addresses;
