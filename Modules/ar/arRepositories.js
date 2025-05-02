6d7eae252a75ca39d9764aa600057778e463615f
ian <ian@isilverstone.com>
2025-05-01 23:59:35 -0400

    style: format code and update copyright notice in arRepositories.js

--- Modules/ar/arRepositories.js
+++ Modules/ar/arRepositories.js
@@ -1,15 +1,15 @@
 'use strict';
 
 /*
-* Copyright © 2024-present, Ian Silverstone
-*
-* See the LICENSE file at the top-level directory of this distribution
-* for licensing information.
-*
-* Removal or modification of this copyright notice is prohibited.
-*/
+ * Copyright © 2024-present, Ian Silverstone
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ */
 
-import Clients from "./models/Clients.js";
+import Clients from './models/Clients.js';
 
 const repositories = {
   clients: Clients,
