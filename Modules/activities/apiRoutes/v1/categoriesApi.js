54e1716e7232950fad3d1734fdab710e94a2f964
ian <ian@isilverstone.com>
2025-05-03 17:48:16 -0400

    feat: add categories API route with CRUD operations

---
+++ Modules/activities/apiRoutes/v1/categoriesApi.js
@@ -0,0 +1,27 @@
+'use strict';
+
+/*
+ * Copyright © 2024-present, Ian Silverstone
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ */
+
+import express from 'express';
+import CategoryController from '../../controllers/CategoryController.js';
+
+const router = express.Router();
+
+router
+  .route('/')
+  .post(CategoryController.create);
+  
+router
+  .route('/:id')
+  .get(CategoryController.getById)
+  .put(CategoryController.update)
+  .delete(CategoryController.remove);
+
+export default router;
