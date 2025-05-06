46a0ffe9dc244450259798f24592f39833be0fc8
ian <ian@isilverstone.com>
2025-05-06 00:32:43 -0400

    feat: add models for LedgerBalances, PostingQueues, ApCreditMemos, ApInvoiceLines, ApInvoices, Payments, ArInvoiceLines, ArInvoices, and Receipts with copyright notices

---
+++ Modules/accounting/models/LedgerBalances.js
@@ -0,0 +1,21 @@
+'use strict';
+
+/*
+* Copyright © 2024-present, Ian Silverstone
+*
+* See the LICENSE file at the top-level directory of this distribution
+* for licensing information.
+*
+* Removal or modification of this copyright notice is prohibited.
+*/
+
+import BaseModel from "pg-schemata";
+import ledgerBalancesSchema from "../schemas/ledgerBalancesScema";
+
+class LedgerBalances extends BaseModel {
+  constructor(db, pgp, logger = null) {
+    super(db, pgp, ledgerBalancesSchema, logger);
+  }
+}
+
+export default LedgerBalances;
