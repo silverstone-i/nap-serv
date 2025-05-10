ad8e98dada84eb4f213cedce752b30509a9c775a
ian <ian@isilverstone.com>
2025-05-10 18:47:06 -0400

    refactor: simplify NapUsersController constructor by removing model parameter

--- Modules/tenants/controllers/NapUsersController.js
+++ Modules/tenants/controllers/NapUsersController.js
@@ -9,13 +9,11 @@
 * Removal or modification of this copyright notice is prohibited.
 */
 
-import { db } from '../../../src/db/db.js';
 import BaseController from '../../../src/utils/BaseController.js';
 
 class NapUsersController extends BaseController {
-  constructor(model = db.napUsers) {
+  constructor() {
     super('napUsers');
-    this.model = model;
   }
 }
 
