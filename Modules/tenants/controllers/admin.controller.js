bad00f50343c9918a03f33cc55b4d4b3bafccf91
ian <ian@isilverstone.com>
2025-07-03 21:30:45 -0400

    chore: add copyright notice to admin.controller.js

--- Modules/tenants/controllers/admin.controller.js
+++ Modules/tenants/controllers/admin.controller.js
@@ -1,3 +1,14 @@
+'use strict';
+
+/*
+* Copyright © 2024-present, Ian Silverstone
+*
+* See the LICENSE file at the top-level directory of this distribution
+* for licensing information.
+*
+* Removal or modification of this copyright notice is prohibited.
+*/
+
 import db from '../../../src/db/db.js';
 
 export async function getAllSchemas(req, res) {
