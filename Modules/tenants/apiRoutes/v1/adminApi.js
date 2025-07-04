f4144b79a39473fe58026fd585b0562b137b1538
ian <ian@isilverstone.com>
2025-07-03 21:28:15 -0400

    chore: add copyright notice to new and existing files

--- Modules/tenants/apiRoutes/v1/adminApi.js
+++ Modules/tenants/apiRoutes/v1/adminApi.js
@@ -1,3 +1,14 @@
+'use strict';
+
+/*
+* Copyright © 2024-present, Ian Silverstone
+*
+* See the LICENSE file at the top-level directory of this distribution
+* for licensing information.
+*
+* Removal or modification of this copyright notice is prohibited.
+*/
+
 import express from 'express';
 import { getAllSchemas, switchSchema } from '../../controllers/admin.controller.js';
 
