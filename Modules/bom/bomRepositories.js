a7a88f7a5143d1f594e73985cdb38d992cdebccb
ian <ian@isilverstone.com>
2025-07-22 01:24:23 -0400

    chore: update copyright notice formatting and maintain code structure

--- Modules/bom/bomRepositories.js
+++ Modules/bom/bomRepositories.js
@@ -1,11 +1,20 @@
 'use strict';
 
 /*
-* Copyright © 2024-present, Ian Silverstone
-*
-* See the LICENSE file at the top-level directory of this distribution
-* for licensing information.
-*
-* Removal or modification of this copyright notice is prohibited.
-*/
+ * Copyright © 2024-present, Ian Silverstone
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ */
 
+import CatalogSkus from './models/CatalogSkus.js';
+import VendorSkus from './models/VendorSkus.js';
+
+const repository = {
+  catalogSkus: CatalogSkus,
+  vendorSkus: VendorSkus,
+};
+
+export default repository;
