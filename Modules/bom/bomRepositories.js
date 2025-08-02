3ca8223c0278aeb529f17fb832bf73afa493f38e
ian <ian@isilverstone.com>
2025-08-02 13:44:00 -0400

    feat: remove deprecated BOM API routes, controllers, models, and schemas

--- Modules/bom/bomRepositories.js
+++ Modules/bom/bomRepositories.js
@@ -9,16 +9,3 @@
  * Removal or modification of this copyright notice is prohibited.
  */
 
-import CatalogSkus from './models/CatalogSkus.js';
-import VendorSkus from './models/VendorSkus.js';
-import EmbeddingSkus from './models/EmbeddingSkus.js';
-import EmbeddingMatches from './models/EmbeddingMatches.js';
-
-const repository = {
-  catalogSkus: CatalogSkus,
-  vendorSkus: VendorSkus,
-  embeddingSkus: EmbeddingSkus,
-  embeddingMatches: EmbeddingMatches,
-};
-
-export default repository;
